d6e5846d9cfb27ca83ff29535724b8d43e853259
Frederic LE BLAY <fle-blay@e1r1p3.42.fr>
2021-12-17 16:07:44 +0100

    Ajout timer

--- Makefile
+++ Makefile
@@ -6,16 +6,16 @@ HEADER = ./includes/solong.h
 
 INCLUDES = ./includes
 
-NAME = solong
+NAME = so_long
 
 all : ${NAME}
 
 ${NAME} : ${OBJS} ${HEADER}
 	make -C ./minilibx/minilibx_opengl
-	gcc -L ./minilibx/minilibx_opengl -lmlx -framework OpenGL -framework AppKit -o ${NAME} ${OBJS}
+	gcc -Wall -Wextra -Werror -L ./minilibx/minilibx_opengl -lmlx -framework OpenGL -framework AppKit -o ${NAME} ${OBJS}
 
 %.o : %.c
-	gcc -c $< -I ${INCLUDES} -o $@
+	gcc -Wall -Wextra -Werror -c $< -I ${INCLUDES} -o $@
 
 clean :
 	rm -rf ${OBJS}
--- solong.c
+++ solong.c
@@ -6,7 +6,7 @@
 /*   By: fle-blay <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/17 10:17:37 by fle-blay          #+#    #+#             */
-/*   Updated: 2021/12/17 12:07:18 by fle-blay         ###   ########.fr       */
+/*   Updated: 2021/12/17 16:06:20 by fle-blay         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,6 +19,7 @@
 typedef struct s_mlx {
 	void	*mlx;
 	void	*win;
+	int		timer;;
 }			t_mlx;
 
 int close_win(int keycode, t_mlx *mlx)
@@ -32,9 +33,40 @@ int close_win(int keycode, t_mlx *mlx)
 	return (0);
 }
 
+int button(int keycode, t_mlx *mlx)
+{
+	(void)mlx;
+	printf("button keycode >%d<\n", keycode);
+	return (0);
+}
+
+int	render(t_mlx *mlx)
+{
+	static int i = 0;
+	if (i == 2400)
+	{
+		printf("Time : %d\n", mlx->timer);
+		i -= 2400;
+		mlx->timer++;
+	}
+	i++;
+	return (0);
+}
+
+/*int mouseh(int keycode, t_mlx *mlx)
+{
+	(void)keycode;
+	printf("ici\n");
+	mlx_mouse_get_pos(mlx->win, &(mlx->mousex), &(mlx->mousey));
+	printf("la\n");
+	printf("mouse x : >%d< mouse y : >%d<\n", mlx->mousex, mlx->mousey);
+	return (0);
+}*/
+
 int main(void)
 {
 	t_mlx	mlx;
+	mlx.timer = 0;
 	void	*img;
 	char 	*path;
 	int		width;
@@ -46,6 +78,10 @@ int main(void)
 	img = mlx_xpm_file_to_image(mlx.mlx, path, &width, &height);
 	mlx_put_image_to_window(mlx.mlx, mlx.win, img, 0, 0);
 	mlx_hook(mlx.win, 2, 1L<<0, close_win, &mlx);
+	mlx_hook(mlx.win, 4, 1L<<2, button, &mlx);
+//	mlx_hook(mlx.win, 6, 0L, mouseh, &mlx);
+	mlx_loop_hook(mlx.mlx, render, &mlx);
+	mlx_loop_hook(mlx.mlx, move, &mlx);
 	mlx_loop(mlx.mlx);
 	return (0);
 }
