3027050295960d87d532e9cab5c9c73580f13418
Frederic LE BLAY <fle-blay@e1r1p10.42.fr>
2021-12-20 16:20:44 +0100

    On bouge dans les 4 directions

--- includes/solong.h
+++ includes/solong.h
@@ -6,7 +6,7 @@
 /*   By: fle-blay <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/20 11:14:11 by fle-blay          #+#    #+#             */
-/*   Updated: 2021/12/20 12:34:54 by fle-blay         ###   ########.fr       */
+/*   Updated: 2021/12/20 16:15:34 by fle-blay         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,12 +19,13 @@ typedef struct s_pers {
 	int		x;
 	int		y;
 	int		mv;
-	//void	*img;
-	void	*d_id[6];
-	void	*u_id[6];
-	void	*l_id[4];
+	int		bsy;
+	int		rev;
+	void	*idl[6];
+	void	*ridl[6];
+	void	*run[6];
+	void	*rrun[6];
 	void	*r_id[4];
-	//char	*pth;
 }			t_pers;
 
 typedef struct s_mlx {
--- solong.c
+++ solong.c
@@ -6,7 +6,7 @@
 /*   By: fle-blay <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/17 10:17:37 by fle-blay          #+#    #+#             */
-/*   Updated: 2021/12/20 12:49:38 by fle-blay         ###   ########.fr       */
+/*   Updated: 2021/12/20 16:19:57 by fle-blay         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,22 +18,26 @@
 int	close_win(int keycode, t_mlx *mlx)
 {
 	printf("keycode >%d<\n", keycode);
-	//if (keycode == 2)
-	//	mlx->hro.x += 16;
-	//if (keycode == 0)
-	//	mlx->hro.x -= 16;
-	//if (keycode == 1)
-	//	mlx->hro.y += 16;
-	//if (keycode == 13)
-	//	mlx->hro.y -= 16;
-	if (keycode == 2)
+	if (keycode == 2 && !mlx->hro.mv)
+	{
 		mlx->hro.mv = 'r';
-	if (keycode == 0)
+		mlx->hro.bsy = 0;
+	}
+	if (keycode == 0 && !mlx->hro.mv)
+	{
 		mlx->hro.mv = 'l';
-	if (keycode == 1)
+		mlx->hro.bsy = 0;
+	}
+	if (keycode == 1 && !mlx->hro.mv)
+	{
 		mlx->hro.mv = 'd';
-	if (keycode == 13)
+		mlx->hro.bsy = 0;
+	}
+	if (keycode == 13 && !mlx->hro.mv)
+	{
 		mlx->hro.mv = 'u';
+		mlx->hro.bsy = 0;
+	}
 	if (keycode == 53)
 	{
 		mlx_destroy_window(mlx->mlx, mlx->win);
@@ -54,20 +58,79 @@ void	movepnj(t_mlx *mlx)
 	t_mlx_fx_img_to_win	fx;
 
 	fx = &mlx_put_image_to_window;
-	//mlx->hro.x += 16;
 	mlx_clear_window(mlx->mlx, mlx->win);
-	if (mlx->hro.mv == 0)
-		fx(mlx->mlx, mlx->win, mlx->hro.d_id[mlx->render % 6], mlx->hro.x, mlx->hro.y);
-	if (mlx->hro.mv == 'r')
-		fx(mlx->mlx, mlx->win, mlx->hro.u_id[mlx->render % 6], mlx->hro.x, mlx->hro.y);
+	if (mlx->hro.mv == 0 && !mlx->hro.rev)
+		fx(mlx->mlx, mlx->win, mlx->hro.idl[mlx->render % 6], mlx->hro.x, mlx->hro.y);
+	if (mlx->hro.mv == 0 && mlx->hro.rev)
+		fx(mlx->mlx, mlx->win, mlx->hro.ridl[mlx->render % 6], mlx->hro.x, mlx->hro.y);
+	if (mlx->hro.mv == 'r' && mlx->hro.bsy != -1)
+	{
+		if (mlx->hro.bsy == 0)
+			mlx->hro.x += (1*4);
+		mlx->hro.x += (3*4);
+		fx(mlx->mlx, mlx->win, mlx->hro.run[mlx->hro.bsy], mlx->hro.x, mlx->hro.y);
+		mlx->hro.bsy++;
+		if (mlx->hro.bsy == 6)
+		{
+			mlx->hro.mv = 0;
+			mlx->hro.bsy = -1;
+			mlx->hro.rev = 0;
+		}
+	}
+	if (mlx->hro.mv == 'l' && mlx->hro.bsy != -1)
+	{
+		if (mlx->hro.bsy == 0)
+			mlx->hro.x -= (1*4);
+		mlx->hro.x -= (3*4);
+		fx(mlx->mlx, mlx->win, mlx->hro.rrun[mlx->hro.bsy], mlx->hro.x, mlx->hro.y);
+		mlx->hro.bsy++;
+		if (mlx->hro.bsy == 6)
+		{
+			mlx->hro.mv = 0;
+			mlx->hro.bsy = -1;
+			mlx->hro.rev = 1;
+		}
+	}
+	if (mlx->hro.mv == 'u' && mlx->hro.bsy != -1)
+	{
+		if (mlx->hro.bsy == 0)
+			mlx->hro.y -= (1*4);
+		mlx->hro.y -= (3*4);
+		if (mlx->hro.rev)
+			fx(mlx->mlx, mlx->win, mlx->hro.rrun[mlx->hro.bsy], mlx->hro.x, mlx->hro.y);
+		else
+			fx(mlx->mlx, mlx->win, mlx->hro.run[mlx->hro.bsy], mlx->hro.x, mlx->hro.y);
+		mlx->hro.bsy++;
+		if (mlx->hro.bsy == 6)
+		{
+			mlx->hro.mv = 0;
+			mlx->hro.bsy = -1;
+		}
+	}
+	if (mlx->hro.mv == 'd' && mlx->hro.bsy != -1)
+	{
+		if (mlx->hro.bsy == 0)
+			mlx->hro.y += (1*4);
+		mlx->hro.y += (3*4);
+		if (mlx->hro.rev)
+			fx(mlx->mlx, mlx->win, mlx->hro.rrun[mlx->hro.bsy], mlx->hro.x, mlx->hro.y);
+		else
+			fx(mlx->mlx, mlx->win, mlx->hro.run[mlx->hro.bsy], mlx->hro.x, mlx->hro.y);
+		mlx->hro.bsy++;
+		if (mlx->hro.bsy == 6)
+		{
+			mlx->hro.mv = 0;
+			mlx->hro.bsy = -1;
+		}
+	}
 }
 
 int	render(t_mlx *mlx)
 {
-	if (mlx->timer == 1024)
+	if (mlx->timer == 512)
 	{
 		printf("Time to render\n");
-		mlx->timer -= 1024;
+		mlx->timer = 0;
 		mlx->render++;
 		movepnj(mlx);
 	}
@@ -83,25 +146,41 @@ int	main(void)
 	fx = &mlx_xpm_file_to_image;
 	mlx.timer = 0;
 	mlx.render = 0;
-	//mlx.hro.pth = "./knight.xpm";
 	mlx.hro.x = 0;
 	mlx.hro.y = 0;
 	mlx.hro.mv = 0;
+	mlx.hro.rev = 0;
 
 	mlx.mlx = mlx_init();
 	mlx.win = mlx_new_window(mlx.mlx, 800, 600, "My window");
-	mlx.hro.d_id[0] = fx(mlx.mlx, "./img/knight_idle_anim_f0.xpm", &(mlx.hro.w), &(mlx.hro.h));
-	mlx.hro.d_id[1] = fx(mlx.mlx, "./img/knight_idle_anim_f1.xpm", &(mlx.hro.w), &(mlx.hro.h));
-	mlx.hro.d_id[2] = fx(mlx.mlx, "./img/knight_idle_anim_f2.xpm", &(mlx.hro.w), &(mlx.hro.h));
-	mlx.hro.d_id[3] = fx(mlx.mlx, "./img/knight_idle_anim_f3.xpm", &(mlx.hro.w), &(mlx.hro.h));
-	mlx.hro.d_id[4] = fx(mlx.mlx, "./img/knight_idle_anim_f4.xpm", &(mlx.hro.w), &(mlx.hro.h));
-	mlx.hro.d_id[5] = fx(mlx.mlx, "./img/knight_idle_anim_f5.xpm", &(mlx.hro.w), &(mlx.hro.h));
-	mlx.hro.u_id[0] = fx(mlx.mlx, "./img/knight_run_anim_f0.xpm", &(mlx.hro.w), &(mlx.hro.h));
-	mlx.hro.u_id[1] = fx(mlx.mlx, "./img/knight_run_anim_f1.xpm", &(mlx.hro.w), &(mlx.hro.h));
-	mlx.hro.u_id[2] = fx(mlx.mlx, "./img/knight_run_anim_f2.xpm", &(mlx.hro.w), &(mlx.hro.h));
-	mlx.hro.u_id[3] = fx(mlx.mlx, "./img/knight_run_anim_f3.xpm", &(mlx.hro.w), &(mlx.hro.h));
-	mlx.hro.u_id[4] = fx(mlx.mlx, "./img/knight_run_anim_f4.xpm", &(mlx.hro.w), &(mlx.hro.h));
-	mlx.hro.u_id[5] = fx(mlx.mlx, "./img/knight_run_anim_f5.xpm", &(mlx.hro.w), &(mlx.hro.h));
+	mlx.hro.idl[0] = fx(mlx.mlx, "./img/big_knight_idle_anim_f0.xpm", &(mlx.hro.w), &(mlx.hro.h));
+	mlx.hro.idl[1] = fx(mlx.mlx, "./img/big_knight_idle_anim_f1.xpm", &(mlx.hro.w), &(mlx.hro.h));
+	mlx.hro.idl[2] = fx(mlx.mlx, "./img/big_knight_idle_anim_f2.xpm", &(mlx.hro.w), &(mlx.hro.h));
+	mlx.hro.idl[3] = fx(mlx.mlx, "./img/big_knight_idle_anim_f3.xpm", &(mlx.hro.w), &(mlx.hro.h));
+	mlx.hro.idl[4] = fx(mlx.mlx, "./img/big_knight_idle_anim_f4.xpm", &(mlx.hro.w), &(mlx.hro.h));
+	mlx.hro.idl[5] = fx(mlx.mlx, "./img/big_knight_idle_anim_f5.xpm", &(mlx.hro.w), &(mlx.hro.h));
+
+	mlx.hro.ridl[0] = fx(mlx.mlx, "./img/revbig_knight_idle_anim_f0.xpm", &(mlx.hro.w), &(mlx.hro.h));
+	mlx.hro.ridl[1] = fx(mlx.mlx, "./img/revbig_knight_idle_anim_f1.xpm", &(mlx.hro.w), &(mlx.hro.h));
+	mlx.hro.ridl[2] = fx(mlx.mlx, "./img/revbig_knight_idle_anim_f2.xpm", &(mlx.hro.w), &(mlx.hro.h));
+	mlx.hro.ridl[3] = fx(mlx.mlx, "./img/revbig_knight_idle_anim_f3.xpm", &(mlx.hro.w), &(mlx.hro.h));
+	mlx.hro.ridl[4] = fx(mlx.mlx, "./img/revbig_knight_idle_anim_f4.xpm", &(mlx.hro.w), &(mlx.hro.h));
+	mlx.hro.ridl[5] = fx(mlx.mlx, "./img/revbig_knight_idle_anim_f5.xpm", &(mlx.hro.w), &(mlx.hro.h));
+
+	mlx.hro.run[0] = fx(mlx.mlx, "./img/big_knight_run_anim_f0.xpm", &(mlx.hro.w), &(mlx.hro.h));
+	mlx.hro.run[1] = fx(mlx.mlx, "./img/big_knight_run_anim_f1.xpm", &(mlx.hro.w), &(mlx.hro.h));
+	mlx.hro.run[2] = fx(mlx.mlx, "./img/big_knight_run_anim_f2.xpm", &(mlx.hro.w), &(mlx.hro.h));
+	mlx.hro.run[3] = fx(mlx.mlx, "./img/big_knight_run_anim_f3.xpm", &(mlx.hro.w), &(mlx.hro.h));
+	mlx.hro.run[4] = fx(mlx.mlx, "./img/big_knight_run_anim_f4.xpm", &(mlx.hro.w), &(mlx.hro.h));
+	mlx.hro.run[5] = fx(mlx.mlx, "./img/big_knight_run_anim_f5.xpm", &(mlx.hro.w), &(mlx.hro.h));
+
+	mlx.hro.rrun[0] = fx(mlx.mlx, "./img/revbig_knight_run_anim_f0.xpm", &(mlx.hro.w), &(mlx.hro.h));
+	mlx.hro.rrun[1] = fx(mlx.mlx, "./img/revbig_knight_run_anim_f1.xpm", &(mlx.hro.w), &(mlx.hro.h));
+	mlx.hro.rrun[2] = fx(mlx.mlx, "./img/revbig_knight_run_anim_f2.xpm", &(mlx.hro.w), &(mlx.hro.h));
+	mlx.hro.rrun[3] = fx(mlx.mlx, "./img/revbig_knight_run_anim_f3.xpm", &(mlx.hro.w), &(mlx.hro.h));
+	mlx.hro.rrun[4] = fx(mlx.mlx, "./img/revbig_knight_run_anim_f4.xpm", &(mlx.hro.w), &(mlx.hro.h));
+	mlx.hro.rrun[5] = fx(mlx.mlx, "./img/revbig_knight_run_anim_f5.xpm", &(mlx.hro.w), &(mlx.hro.h));
+
 	mlx_hook(mlx.win, 2, 1L << 0, close_win, &mlx);
 	mlx_hook(mlx.win, 4, 1L << 2, button, &mlx);
 	mlx_loop_hook(mlx.mlx, render, &mlx);
